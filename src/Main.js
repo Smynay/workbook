530baa8e52c6d4f96b2c7c957f28b0481956b7b8
Smynay <smynay@yandex.ru>
2020-11-17 15:21:45 +0300

    useReducer

--- src/Main.js
+++ src/Main.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useAlert } from './alert/AlertContext';
 
 export default function Main() {
-  const { toggle } = useAlert();
+  const { show } = useAlert();
   return (
     <div>
       <h1>Context hello</h1>
-      <button className="btn btn-success" onClick={toggle}>
+      <button className="btn btn-success" onClick={() => show('Main.js text')}>
         Show alert
       </button>
     </div>
