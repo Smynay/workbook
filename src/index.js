4c9f18a9e6e8a7c14699e82cbdbb043b36be862d
Nikita Schastnyy <smynay@yandex.ru>
2021-01-14 13:06:57 +0300

    Second lesson

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+const starWarsChars = [
+  { name: 'Dart Vaider', side: 'dark' },
+  { name: 'Luk Skywalker', side: 'light' },
+  { name: 'Palpatin', side: 'dark' },
+  { name: 'Obi Van Kanobi', side: 'light' },
+];
+
+function App({ list, side = 'light' }) {
+  const filteredList = list.filter((char) => char.side === side);
+  return (
+    <div className="App">
+      <ul>
+        {filteredList.map((char, index) => (
+          <li key={char.name + index}>
+            <strong>{char.name} - </strong>
+            {char.side}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App list={starWarsChars} />
   </React.StrictMode>,
   document.getElementById('root')
 );
